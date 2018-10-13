73e0a48e94e921c4b353de558c5e960afb189f88
Михаил Паниковский <panikowsky.m@gmail.com>
2018-10-13 23:19:01 +0300

    Добавьте файлы проекта.

---
+++ Lab3/Form1.Designer.cs
@@ -0,0 +1,401 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.ForeColor = System.Drawing.Color.Gray;
+            this.textBox1.Location = new System.Drawing.Point(3, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox2.ForeColor = System.Drawing.Color.Gray;
+            this.textBox2.Location = new System.Drawing.Point(143, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(134, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 317);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Управление";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "b";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "a";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button17);
+            this.panel1.Controls.Add(this.button16);
+            this.panel1.Controls.Add(this.button15);
+            this.panel1.Controls.Add(this.groupBox4);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Location = new System.Drawing.Point(6, 55);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(281, 256);
+            this.panel1.TabIndex = 3;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(6, 133);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(33, 23);
+            this.button17.TabIndex = 3;
+            this.button17.Text = "tan";
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(6, 162);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(33, 23);
+            this.button16.TabIndex = 3;
+            this.button16.Text = "sin";
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(6, 104);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(33, 23);
+            this.button15.TabIndex = 3;
+            this.button15.Text = "ln";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button14);
+            this.groupBox4.Controls.Add(this.button10);
+            this.groupBox4.Controls.Add(this.button13);
+            this.groupBox4.Controls.Add(this.button11);
+            this.groupBox4.Controls.Add(this.button12);
+            this.groupBox4.Location = new System.Drawing.Point(81, 94);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(125, 83);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Операции сравн-я";
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(23, 48);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 23);
+            this.button14.TabIndex = 7;
+            this.button14.Text = "равенство";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(39, 19);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(19, 23);
+            this.button10.TabIndex = 3;
+            this.button10.Text = ">";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(6, 19);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(27, 23);
+            this.button13.TabIndex = 6;
+            this.button13.Text = ">=";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(66, 19);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(20, 23);
+            this.button11.TabIndex = 4;
+            this.button11.Text = "<";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(90, 19);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(29, 23);
+            this.button12.TabIndex = 5;
+            this.button12.Text = "<=";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(3, 10);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(131, 78);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Арифм. операции";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 22);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(78, 32);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(42, 22);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "mod";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 47);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(30, 22);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "/";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(42, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(42, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 22);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "x";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button8);
+            this.groupBox3.Controls.Add(this.button9);
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Location = new System.Drawing.Point(140, 10);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(131, 78);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Логич. операции";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(95, 32);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(29, 23);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "xor";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(7, 46);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(42, 23);
+            this.button9.TabIndex = 3;
+            this.button9.Text = "not";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(50, 46);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(43, 23);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "or";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 19);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(87, 22);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "and";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(329, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Результаты";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(332, 28);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(238, 301);
+            this.textBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 388);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Lab3/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, res;
+            if (int.TryParse(textBox1.Text, out a) && int.TryParse(textBox2.Text, out b))
+            {
+                res = a + b;
+                textBox3.Text = "+(a,b)= " + res.ToString();
+            }
+            else MessageBox.Show("Невозможно выполнить операцию!\r\n" +
+                             "Проверьте аргументы.", "Проверьте ввод !", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a, b, res;
+            if (int.TryParse(textBox1.Text, out a) && int.TryParse(textBox2.Text, out b))
+            {
+                res = a - b;
+                textBox3.Text = "-(a,b)= " + res.ToString();
+            }
+            else MessageBox.Show("Ошибка ввода!\r\n" +
+                             "Недопустимые а и б.","Недоп-е аргументы" , MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int a, b, res;
+            if (int.TryParse(textBox1.Text, out a) && int.TryParse(textBox2.Text, out b))
+            {
+                if (a == 0 && b == 0) { MessageBox.Show("0/0 Неопределено", "Неопределенность!", MessageBoxButtons.OK); return; }
+                if(b == 0) { MessageBox.Show("const/0 = inf", "Деление на 0", MessageBoxButtons.OK); return; }
+                res = a/b;
+                textBox3.Text = "/(a,b)= " + res.ToString();
+            }
+            else MessageBox.Show("Недопустимые аргументы!\r\n" +
+                             "Проверьте а и б.", "Ошибка !", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a, b, res;
+            if (int.TryParse(textBox1.Text, out a) && int.TryParse(textBox2.Text, out b))
+            {
+                if (a == 0 && b == 0) { MessageBox.Show("0/0 Неопределено", "Неопределенность!", MessageBoxButtons.OK); return; }
+                if (b == 0) { MessageBox.Show("const/0 = inf", "Деление на 0", MessageBoxButtons.OK); return; }
+                res = a * b;
+                textBox3.Text = "multi(a,b)= " + res.ToString();
+            }
+            else MessageBox.Show("Недопустимые аргументы!\r\n" +
+                             "Проверьте а и б.", "Ошибка !", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int a, b, res;
+            if (int.TryParse(textBox1.Text, out a) && int.TryParse(textBox2.Text, out b))
+            {
+                res = a%b;
+                textBox3.Text = "mod(a,b)= " + res.ToString();
+            }
+            else MessageBox.Show("Ошибка ввода!\r\n" +
+                             "Проверьте а и б.", "Недоп-е аргументы!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+           
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            if (int.TryParse(textBox1.Text, out a) && int.TryParse(textBox2.Text, out b))
+            {
+                if (a == b) { textBox3.Text = "==(" + a.ToString() + "," + b.ToString() + "): true"; }
+                else textBox3.Text = "==(" + a.ToString() + "," + b.ToString() + "): false";
+            }
+            else MessageBox.Show("Недопустимые аргументы!\r\n" +
+                             "Проверьте а и б.", "Ошибка !", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int a, b;
+
+            if (int.TryParse(textBox1.Text, out a) && int.TryParse(textBox2.Text, out b))
+            {
+                if (a > b) {textBox3.Text = ">(" + a.ToString() +"," + b.ToString() + "): true"+"\r\n"; }
+            else textBox3.Text = ">("+a.ToString()+","+b.ToString()+"): false";
+            }
+            else MessageBox.Show("Недопустимые аргументы!\r\n" +
+                             "Проверьте а и б.", "Ошибка !", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            int a, b;
+
+            if (int.TryParse(textBox1.Text, out a) && int.TryParse(textBox2.Text, out b))
+            {
+                if (a < b) { textBox3.Text = "<(" + a.ToString() + "," + b.ToString() + "): true"+"\r\n"; }
+                else textBox3.Text = "<(" + a.ToString() + "," + b.ToString() + "): false"+"\r\n";
+            }
+            else MessageBox.Show("Недопустимые аргументы!\r\n" +
+                             "Проверьте а и б.", "Ошибка !", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+
+    }
